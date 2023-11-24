9b802d50823d6545aaef80c5040e24b678e8e42b
Mona <monabadee@gmail.com>
2023-11-24 15:21:34 +0300

    Complete  Lab

--- index.js
+++ index.js
@@ -1,1 +1,74 @@
-// Your code here
+// Helper function to create an employee record
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    return {
+      firstName,
+      familyName,
+      title,
+      payPerHour,
+      timeInEvents: [],
+      timeOutEvents: [],
+    };
+  }
+  
+  // Helper function to create employee records from an array of arrays
+  function createEmployeeRecords(employeeData) {
+    return employeeData.map(createEmployeeRecord);
+  }
+  
+  // Helper function to create a timeIn event
+  function createTimeInEvent(employee, dateTime) {
+    const [date, hour] = dateTime.split(" ");
+    employee.timeInEvents.push({
+      type: "TimeIn",
+      date,
+      hour: parseInt(hour, 10),
+    });
+    return employee;
+  }
+  
+  // Helper function to create a timeOut event
+  function createTimeOutEvent(employee, dateTime) {
+    const [date, hour] = dateTime.split(" ");
+    employee.timeOutEvents.push({
+      type: "TimeOut",
+      date,
+      hour: parseInt(hour, 10),
+    });
+    return employee;
+  }
+  
+  // Helper function to calculate hours worked on a specific date
+  function hoursWorkedOnDate(employee, date) {
+    const timeInEvent = employee.timeInEvents.find((event) => event.date === date);
+    const timeOutEvent = employee.timeOutEvents.find((event) => event.date === date);
+    return (timeOutEvent.hour - timeInEvent.hour) / 100;
+  }
+  
+  // Helper function to calculate wages earned on a specific date
+  function wagesEarnedOnDate(employee, date) {
+    const hoursWorked = hoursWorkedOnDate(employee, date);
+    return hoursWorked * employee.payPerHour;
+  }
+  
+  // Helper function to calculate all wages for an employee
+  function allWagesFor(employee) {
+    const datesWorked = employee.timeInEvents.map((event) => event.date);
+    return datesWorked.reduce((totalWages, date) => totalWages + wagesEarnedOnDate(employee, date), 0);
+  }
+  
+  // Helper function to calculate payroll for an array of employees
+  function calculatePayroll(employees) {
+    return employees.reduce((totalPayroll, employee) => totalPayroll + allWagesFor(employee), 0);
+  }
+  
+  // Example usage:
+  const employeeData = [
+    ["Gray", "Worm", "Security", 1],
+    ["Thor", "Odinsson", "Electrical Engineer", 45],
+    ["Loki", "Laufeysson-Odinsson", "HR Representative", 35],
+    // Add more employee data as needed...
+  ];
+  
+  const employees = createEmployeeRecords(employeeData);
+  
+ 
